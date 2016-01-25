66303082dda6c3eb0410b648febda5708e225c1d
mvfontes <mfontes@thoughtworks.com>
2016-01-25 18:34:44 -0300

    Add some tests to places_test

--- app/places/places.js
+++ app/places/places.js
@@ -5,10 +5,17 @@ angular.module('myApp.places', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/places', {
     templateUrl: 'places/places.html',
-    controller: 'PlacesCtrl'
+    controller: 'PlacesCtrl as ctrl'
   });
 }])
 
 .controller('PlacesCtrl', [function() {
+	var self = this;
+	self.search_string = "";
+
+	self.search = function (){
+		console.log('fui la na API do google e pesquisei por: ' + self.search_string);
+		// places = API.search()
+	}
 
 }]);
--- webclient-project/e2e-tests/scenarios.js
+++ webclient-project/e2e-tests/scenarios.js
@@ -17,9 +17,25 @@ describe('my app', function() {
       browser.get('index.html#/places');
     });
 
-    it('should render a input form for search places when user navigates to /places', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+    it('should render an input form for search places when user navigates to /places', function() {
+      var search_input = element(by.tagName('input'));
+      search_input.sendKeys('pote');
+
+      var search_button = element(by.tagName('button'));
+      search_button.click();
+
+      var sendKeysTest = 'results';
+      element(by.tagName('textarea')).sendKeys(sendKeysTest); 
+      // var result = 'oi';
+      // element(by.name('search_result')).getAttribute().then(function(value){result = value; });
+    //  console.log(search_result);
+      // expect(result).toEqual('results');
+
+      expect(element(by.name('search_result')).getAttribute('value')).toEqual(sendKeysTest);
+
+      var list = element.all(by.tagName('ul'));
+      expect(list.count()).toBe(2);
+
     });
 
   });
