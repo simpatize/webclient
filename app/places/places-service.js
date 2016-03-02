c09c77c374e029c244673dc56fbf91f7bb558039
Felipe Martins <ffmmjj@gmail.com>
2016-03-02 16:39:07 -0300

    Story-52 [Marcela, Felipe] Removing calls to console.log(used to check the behavior of angular-environment on Heroku) in the Places service

--- app/places/places-service.js
+++ app/places/places-service.js
@@ -5,8 +5,6 @@ angular
 	.factory('PlacesService', PlacesService);
 
 function PlacesService($resource, envService) {
-	console.log(envService.get());
-	console.log(envService.read('baseBackendUrl') + '/places');
 	return $resource(
 		envService.read('baseBackendUrl') + '/places',
 		{},
