e111503108be85a127fb5dc73b0f5130abe26354
Felipe Martins <ffmmjj@gmail.com>
2016-03-04 16:58:50 -0300

    Story-52 [Felipe] Replaced phantomjs with phantomjs-prebuilt in package.json and added env var pointing to the appropriate phantomjs binary

--- scripts/set_vagrant_env_vars.sh
+++ scripts/set_vagrant_env_vars.sh
@@ -4,3 +4,6 @@ source ~/.bash_profile
 if [ -z "$PORT" ]; then # only checks if VAR is set, regardless of its value
     echo "export PORT=8000" >> /home/vagrant/.bash_profile
 fi
+if [ -z "$PHANTOMJS_BIN" ]; then 
+    echo "export PHANTOMJS_BIN=/home/vagrant/webclient/node_modules/phantomjs-prebuilt/bin/phantomjs" >> /home/vagrant/.bash_profile
+fi
